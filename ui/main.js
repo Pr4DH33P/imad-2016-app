1f85afb9a3a1309ef92e56b57e336c629c1f227d
Pradheep <npradheep135@gmail.com>
2016-09-27 22:28:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,12 +10,19 @@ img.onclick = function() {
 };*/
 
 var button = document.getElementById('counter');
-var counter =0;
 button.onclick = function() {
+  var request = new XMLHttpRequest();
   
-  
-  counter =counter + 1;
-  var span =document.getElementById('count');
-  span.innerHTML = counter.toString();
+  req.onReadyStateChange = function() {
+      if(request.readyState === XMLHttpRequest.Done){
+          if(request.status ===200 ){
+              var counter = request.responseText;
+              var span =document.getElementById('count');
+              span.innerHTML = counter.toString();
+          }
+      }
+  };
+  request.open('GET','http://pr4dh33p.imad.hasura-app.io/counter',true);
+  request.send(null);
 };
 
