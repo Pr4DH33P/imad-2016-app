ed24cb5eddbc75b67c514fa5eb8b36e26202b2b1
Pradheep <npradheep135@gmail.com>
2016-09-27 20:50:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,10 @@
 console.log('Loaded!');
 var img = document.getElementById('img');
+var marginLeft =0;
+function moveright(){
+  marginLeft = marginLeft + 5
+   img.style.marginLeft= marginLeft + 'px' 
+}
 img.onclick = function() {
-    img.style.marginLeft='30px'
+    var interval = setInterval(moveright,100);
 };
