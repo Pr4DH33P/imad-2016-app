db1646e51c8e251b6c52042c5c33b2b9d7228e9a
Pradheep <npradheep135@gmail.com>
2016-10-29 20:02:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
-
 var app = express();
 app.use(morgan('combined'));
-
 var config = {
   host: 'db.imad.hasura-app.io',
   port: '5432',
@@ -13,7 +11,6 @@ var config = {
   password: process.env.DB_PASSWORD,
   database: 'pr4dh33p',
 };
-
       function createTemplate (data) {
             var title = data.title;
             var heading = data.heading;
@@ -22,42 +19,35 @@ var config = {
                         `<html>
                         <head>
                         <title>${title}</title>
-                        <meta name="viewport" content="width-device-width, initial-scale=1" />
-                        <link href="/ui/style.css" rel="stylesheet" />
+                           <meta name="viewport" content="width-device-width, initial-scale=1" />
+                           <link href="/ui/style.css" rel="stylesheet" />
                         </head>
                         <body>
                         <div class="container">
-                         <a href='/'>Home</a>&nbsp;&nbsp;&nbsp;<a href='/Profile'>Profile</a>&nbsp;&nbsp;&nbsp;
-                        <a href='/Contact'>Contact</a>&nbsp;&nbsp;&nbsp;
-                        <hr>
-                         <h1>
-                        ${heading}</h1>
-                        <div>
-                            ${content}
-                        </div>
+                           <a href='/'>Home</a>&nbsp;&nbsp;&nbsp;<a href='/Profile'>Profile</a>&nbsp;&nbsp;&nbsp;
+                           <a href='/Contact'>Contact</a>&nbsp;&nbsp;&nbsp;<hr>
+                        <h1>${heading}</h1>
+                        <div>${content}</div>
                         <hr>
                         </div>
-                        
-                       <!-- <div class='footer'>
-                         <input type='text' id='cmnt_box' placeholder='Type your comment'> </input>
-                         <input type='submit' value='comment' id='cmnt_btn' > </input>
-                         <ul id='cmntlist'></ul>
+                        <!-- <div class='footer'>
+                           <input type='text' id='cmnt_box' placeholder='Type your comment'> </input>
+                           <input type='submit' value='comment' id='cmnt_btn' > </input>
+                           <ul id='cmntlist'></ul>
                         </div> -->
                         </body>
                         </html>`;
         return pageTemplate;
         }
-
 /*var comments = [];
 app.get('/comment', function (req, res) {
   var comment = req.query.comment;
   comments.push(comment);
   res.send(JSON.stringify(comments));
 });*/
-
 var pool = new Pool(config);
 app.get('/db', function (req, res) {
-  pool.query('SELECT * FROM user_data', function (err,result) {
+    pool.query('SELECT * FROM user_data', function (err,result) {
       if (err){
           res.status(500).send(err.toString());
       }
@@ -66,30 +56,24 @@ app.get('/db', function (req, res) {
       }
   });
 });
-
-
 /*var names = [];
 app.get('/submit-name', function (req, res) {
   var name = req.query.name;
   names.push(name);
   res.send(JSON.stringify(names));
-});*/
-
-/*var counter = 0;
+});
+var counter = 0;
 app.get('/counter', function (req, res) {
   counter = counter + 1;
   res.send(counter.toString());
 });*/
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
 app.get('/ui/main.js', function (req, res) {
  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
@@ -102,19 +86,17 @@ app.get('/ui/madi.png', function (req, res) {
 app.get('/:pageName',function (req, res){
     pool.query("SELECT * FROM page where Title ='" + req.params.pageName + "'", function(err,result){
          if (err){
-          res.status(500).send(err.toString());
+         res.status(500).send(err.toString());
          }
       else{
           if(result.rows.length === 0){
           res.status(404).send('404! Page not found!');    
           }
-          else{
-          var pageData= result.rows[0];
-          res.send(createTemplate(pageData));}}
+             else{
+              var pageData= result.rows[0];
+              res.send(createTemplate(pageData));}}
     });
-    
 });
-
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
