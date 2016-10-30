26936d24ce72be3d1abf6a642660323bf42985bb
Pradheep <npradheep135@gmail.com>
2016-10-30 14:13:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -119,11 +119,7 @@ app.get('/:pageName',function (req, res){
               res.send(createTemplate(pageData));}}
     });
 });
-var usrname = document.getElementById('usrname').value;
-var email = document.getElementById('email').value;
-var age = document.getElementById('age').value;
-var password = document.getElementById('password').value;
-var submit = document.getElementById('submit_btn');
+
 submit.onclick = function () {
     pool.query(`INSERT INTO "user" ("name", "age", "email") VALUES ('asfgsdfg', '54', 'sfsdfgf');`);
  };
