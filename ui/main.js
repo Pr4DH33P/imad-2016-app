04f1feb1ac9880f6417742e1b94208995329c76e
Pradheep <npradheep135@gmail.com>
2016-10-30 14:42:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,9 @@
-var Pool = require('pg').Pool;
 var usrname = document.getElementById('usrname').value;
 var email = document.getElementById('email').value;
 var age = document.getElementById('age').value;
 var password = document.getElementById('password').value;
-
 var submit_btn = document.getElementById('submit_btn');
-var pool1 = new Pool(config);
+var pool = new Pool(config);
 submit_btn.onclick = function () {
-    pool1.query(`INSERT INTO "user" ("name", "age", "email") VALUES ('asfgsdfg', '54', 'sfsdfgf');`);
+    pool.query(`INSERT INTO "user" ("name", "age", "email") VALUES ('asfgsdfg', '54', 'sfsdfgf');`);
  };
