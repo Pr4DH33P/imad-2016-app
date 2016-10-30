9a98908da2d5b8b51df47333867519f15bb84a69
Pradheep <npradheep135@gmail.com>
2016-10-30 16:16:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,5 +6,6 @@ var submit_btn = document.getElementById('submit_btn');
 var form = document.getElementById("form");
 document.getElementById("submit_btn").addEventListener("click", function () {
   form.submit();
-  form.pool.query(`INSERT INTO "user" ("name", "age", "email") VALUES ('asfgsdfg', '54', 'sfsdfgf');`);
+  alert("$name");
+    
 });
