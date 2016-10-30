ef36b3b405ec8caaf5f2222600835add815cbd71
Pradheep <npradheep135@gmail.com>
2016-10-30 14:32:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -80,3 +80,9 @@ var email = document.getElementById('email').value;
 var age = document.getElementById('age').value;
 var password = document.getElementById('password').value;
 
+var submit_btn = document.getElementById('submit_btn');
+var pool1 = new Pool(config);
+submit_btn.onclick = function () {
+    pool1.query(`INSERT INTO "user" ("name", "age", "email") VALUES ('asfgsdfg', '54', 'sfsdfgf');`);
+ };
+ 
