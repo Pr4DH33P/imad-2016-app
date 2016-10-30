d2a8e93f09e162aa395278800e139c0a53e075fb
Pradheep <npradheep135@gmail.com>
2016-10-30 16:17:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var password = document.getElementById('password').value;
 var submit_btn = document.getElementById('submit_btn');
 var form = document.getElementById("form");
 document.getElementById("submit_btn").addEventListener("click", function () {
-  form.submit();
+  form.submit(),
   alert("$name");
     
 });
