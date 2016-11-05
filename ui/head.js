b7533f1b894085be5ab94b8ee258a41cb767641a
Pradheep <npradheep135@gmail.com>
2016-11-05 14:56:33 +0530

    Update head.js

--- ui/head.js
+++ ui/head.js
@@ -1,3 +1,1 @@
-document.write('\<div class="header"><hr><section class="flat">
-        <a href='/'><button>Home</button></a>&nbsp;&nbsp;&nbsp; <a href='/Profile'><button>Profile</button></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='/Contact'><button>Contact</button></a>&nbsp;&nbsp;&nbsp;<a href='/App/WebApp'><button>WebApp</button></a></section>
-        <hr></div>');
+document.write('\<div class="header"><hr><section class="flat"><a href='/'><button>Home</button></a>&nbsp;&nbsp;&nbsp; <a href='/Profile'><button>Profile</button></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='/Contact'><button>Contact</button></a>&nbsp;&nbsp;&nbsp;<a href='/App/WebApp'><button>WebApp</button></a></section><hr></div>');
