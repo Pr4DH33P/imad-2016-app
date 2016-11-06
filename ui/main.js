8bc97057968a6aedf66f19b5893257abd148b9c0
Pradheep <npradheep135@gmail.com>
2016-11-06 15:37:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,5 @@
-
-
-var submit = document.getElementById('submit_btn');
-    submit.onclick = function () {
+    var register = document.getElementById('submit_btn');
+    register.onclick = function () {
         // Create a request object
         var request = new XMLHttpRequest();
         
@@ -10,19 +8,13 @@ var submit = document.getElementById('submit_btn');
           if (request.readyState === XMLHttpRequest.DONE) {
               // Take some action
               if (request.status === 200) {
-                  submit.value = 'Sucess!';
-              } else if (request.status === 403) {
-                  submit.value = 'Invalid credentials. Try again?';
-              } else if (request.status === 500) {
-                  alert('Something went wrong on the server');
-                  submit.value = 'Login';
+                  alert('User created successfully');
+                  register.value = 'Registered!';
               } else {
-                  alert('Something went wrong on the server');
-                  submit.value = 'Login';
+                  alert('Could not register the user');
+                  register.value = 'Register';
               }
-              loadLogin();
-          }  
-          // Not done yet
+          }
         };
         
         // Make the request
@@ -30,9 +22,9 @@ var submit = document.getElementById('submit_btn');
         var password = document.getElementById('password').value;
         console.log(username);
         console.log(password);
-        request.open('POST', '/login', true);
+        request.open('POST', '/create-user', true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({username: username, password: password}));  
-        submit.value = 'Logging in...';
-        
+        register.value = 'Registering...';
+    
     };
