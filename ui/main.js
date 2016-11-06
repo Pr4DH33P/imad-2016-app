f627417c294b0e248363b90d9534e02fd7921f69
Pradheep <npradheep135@gmail.com>
2016-11-06 15:51:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
-    var register = document.getElementById('submit_btn');
-    register.onclick = function () {
+ var submit = document.getElementById('login_btn');
+    submit.onclick = function () {
         // Create a request object
         var request = new XMLHttpRequest();
         
@@ -8,13 +8,19 @@
           if (request.readyState === XMLHttpRequest.DONE) {
               // Take some action
               if (request.status === 200) {
-                  alert('User created successfully');
-                  register.value = 'Registered!';
+                  submit.value = 'Sucess!';
+              } else if (request.status === 403) {
+                  submit.value = 'Invalid credentials. Try again?';
+              } else if (request.status === 500) {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
               } else {
-                  alert('Could not register the user');
-                  register.value = 'Register';
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
               }
-          }
+              loadLogin();
+          }  
+          // Not done yet
         };
         
         // Make the request
@@ -22,9 +28,9 @@
         var password = document.getElementById('password').value;
         console.log(username);
         console.log(password);
-        request.open('POST', '/create-user', true);
+        request.open('POST', '/login', true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({username: username, password: password}));  
-        register.value = 'Registering...';
-    
+        submit.value = 'Logging in...';
+        
     };
