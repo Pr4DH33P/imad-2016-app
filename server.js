ccf7ddd6423bda5e100eed3f1174e5fb138451a5
Pradheep <npradheep135@gmail.com>
2016-11-06 14:44:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,13 +76,12 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/App/WebApp', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'app.html'));
-});
 
 app.get('/ui/head.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'head.html'));
 });
+
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
