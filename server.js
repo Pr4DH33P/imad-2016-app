5067fb4d3fe9c762a5193aa4443a5bb3fc5ba153
Pradheep <npradheep135@gmail.com>
2016-11-24 13:37:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -34,78 +34,53 @@ app.use(session({
                            <meta name="viewport" content="width-device-width, initial-scale=1" />
                            <link href="/ui/style.css" rel="stylesheet" />
                            <link href="/ui/style.css" rel="stylesheet" />
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+                            <meta name="viewport" content="width=device-width, initial-scale=1">
+                            <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
                         </head>
                         <body>
-                        
-                        
-                       
                        <div class="w3-container w3-blue">
                        <div id="myModal" class="modal">
-       <div class="modal-content">
-    <div class="modal-header">
-      <span class="close">&times</span>
-      <h2>Menu</h2>
-      </div>
-      <div class="modal-body">
-       <!--<div style="background-color:#aaa;height: 100%;width: 30%;float:left;margin-left: -2%;position: absolute;margin-top: -1.3%;">-->
-         <section class="flat"><ul>
-            <li style="padding-bottom: 2%;"><a href='/'><button>Home</button></a></li>
-            <li style="padding-bottom: 2%;"><a href='/Profile'><button>Profile</button></a></li>
-            <li style="padding-bottom: 2%;"><a href='/Contact'><button>Contact</button></a></li>
-            <li style="padding-bottom: 2%;"><a href='/WebApp'><button>WebApp</button></a></li>
-         </ul></section>
-        </div></div></div> 
-        
-        <div class="top">
-            <div align='left' >
-            <input type="image" id="myBtn" src="https://cdn4.iconfinder.com/data/icons/wirecons-free-vector-icons/32/menu-alt-512.png" width= initial position= fixed margin-left= 1% class=menu /></div> 
-            <h3 align=center>${heading}</h3>
-
-        </div> </div>
-            
-                       
-        
-                        
-                        
-                        
-                        
+                        <div class="modal-content">
+                        <div class="modal-header">
+                        <span class="close">&times</span>
+                        <h2>Menu</h2>
+                        </div>
+                        <div class="modal-body">
+                         <section class="flat"><ul>
+                        <li style="padding-bottom: 2%;"><a href='/'><button>Home</button></a></li>
+                        <li style="padding-bottom: 2%;"><a href='/Profile'><button>Profile</button></a></li>
+                        <li style="padding-bottom: 2%;"><a href='/Contact'><button>Contact</button></a></li>
+                        <li style="padding-bottom: 2%;"><a href='/WebApp'><button>WebApp</button></a></li>
+                        </ul></section>    
+                        </div></div></div> 
+                        <div class="top">
+                        <div align='left' >
+                        <input type="image" id="myBtn" src="https://cdn4.iconfinder.com/data/icons/wirecons-free-vector-icons/32/menu-alt-512.png" width= initial position= fixed margin-left= 1% class=menu /></div> 
+                        <h3 align=center>${heading}</h3>
+                        </div> </div>
                         <div class='container'>${content}</div>
-                        
                         </div>
                         <!-- <div class='footer'>
                            <input type='text' id='cmnt_box' placeholder='Type your comment'> </input>
                            <input type='submit' value='comment' id='cmnt_btn' > </input>
                            <ul id='cmntlist'></ul>
                         </div> -->
-                        
-                     
-    <script>// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}</script>
+                        <script>
+                        var modal = document.getElementById('myModal');
+                        var btn = document.getElementById("myBtn");
+                        var span = document.getElementsByClassName("close")[0];
+                        btn.onclick = function() {
+                        modal.style.display = "block";
+                        }
+                        span.onclick = function() {
+                        modal.style.display = "none";
+                        }   
+                        window.onclick = function(event) {
+                        if (event.target == modal) {
+                        modal.style.display = "none";
+                        }  
+                        }
+                        </script>
                         </body>
                         </html>`;
         return pageTemplate;
