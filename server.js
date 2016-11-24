1875b42486a69441fa2e4fc551c03cd36c561321
Pradheep <npradheep135@gmail.com>
2016-11-24 16:25:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -34,7 +34,6 @@ app.use(session({
                            <meta name="viewport" content="width-device-width, initial-scale=1" />
                             <meta name="theme-color" content="#2196F3" />
                            <link href="/ui/style.css" rel="stylesheet" />
-                           <link href="/ui/style.css" rel="stylesheet" />
                             <meta name="viewport" content="width=device-width, initial-scale=1">
                             <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
                         </head>
