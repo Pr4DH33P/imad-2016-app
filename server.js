538c4d7863673e6415cf3957a10278fdb4052a00
Pradheep <npradheep135@gmail.com>
2016-11-26 18:01:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,20 +94,7 @@ app.use(session({
         }
         
 
-app.get('/check-login', function (req, res) {
-   if (req.session && req.session.auth && req.session.auth.userId) {
-       // Load the user object
-       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
-           if (err) {
-              res.status(500).send(err.toString());
-           } else {
-              res.send(result.rows[0].username);    
-           }
-       });
-   } else {
-       res.status(400).send('You are not logged in');
-   }
-});
+
 
 
 /*var comments = [];
@@ -248,6 +235,20 @@ app.get('/:pageName',function (req, res){
 
 
 
+app.get('/check-login', function (req, res) {
+   if (req.session && req.session.auth && req.session.auth.userId) {
+       // Load the user object
+       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
+           if (err) {
+              res.status(500).send(err.toString());
+           } else {
+              res.send(result.rows[0].username);    
+           }
+       });
+   } else {
+       res.status(400).send('You are not logged in');
+   }
+});
 
 
 app.get('/get-comments/:pageName', function (req, res) {
