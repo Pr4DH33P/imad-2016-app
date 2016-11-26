51adb825d961cee3ba2409bf757fe6850f50f9f3
Pradheep <npradheep135@gmail.com>
2016-11-26 21:12:03 +0530

    [imad-console] Updates ui/head.js

--- ui/head.js
+++ ui/head.js
@@ -99,9 +99,6 @@ function loadComments () {
     request.send(null);
 }
 
-function logout(){
-     content += `<div><a href"/logout">Logout</a></div>`
-}
 
 // The first thing to do is to check if the user is logged in!
 loadLogin();
