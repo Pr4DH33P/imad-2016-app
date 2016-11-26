7e1a8b3dc62b6eaec9ffea2005ae273d18fab19a
Pradheep <npradheep135@gmail.com>
2016-11-26 17:54:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,6 +94,22 @@ app.use(session({
         }
         
 
+app.get('/check-login', function (req, res) {
+   if (req.session && req.session.auth && req.session.auth.userId) {
+       // Load the user object
+       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
+           if (err) {
+              res.status(500).send(err.toString());
+           } else {
+              res.send(result.rows[0].username);    
+           }
+       });
+   } else {
+       res.status(400).send('You are not logged in');
+   }
+});
+
+
 /*var comments = [];
 app.get('/comment', function (req, res) {
   var comment = req.query.comment;
@@ -191,20 +207,7 @@ app.post('/login', function (req, res) {
    });
 });
 
-app.get('/check-login', function (req, res) {
-   if (req.session && req.session.auth && req.session.auth.userId) {
-       // Load the user object
-       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
-           if (err) {
-              res.status(500).send(err.toString());
-           } else {
-              res.send(result.rows[0].username);    
-           }
-       });
-   } else {
-       res.status(400).send('You are not logged in');
-   }
-});
+
 
 app.get('/logout', function (req, res) {
    delete req.session.auth;
